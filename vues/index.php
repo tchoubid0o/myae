35b91a0c76fd0f5247fdeff63ae1afa9b4787d88
= <michael.rupp@hei.fr>
2014-10-31 17:45:09 +0100

    Index

--- vues/index.php
+++ vues/index.php
@@ -8,3 +8,24 @@
 		</button>
 	</p>
 </div>
+<div class="row row-offcanvas row-offcanvas-left" style="margin-left: 0px;margin-right: 0px;">
+
+    <!-- sidebar -->
+    <div class="col-xs-6 col-sm-2 navbar-default sidebar" id="sidebar" role="navigation" style="min-height: 500px;">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+              <li><a href="#">Link 3</a></li>              
+          </ul>
+      </div>
+  </div>
+
+  <!-- main area -->
+  <div class="col-xs-12 col-sm-10" id="page-wrapper"  style="min-height: 500px;">
+      <h1 class="page-header" style="margin-top: 10px;"><span class="glyphicon glyphicon-flag"></span>&nbsp;Les Règles</h1>
+      <p><span class="glyphicon glyphicon-chevron-right"></span>&nbsp; Chiffre d'affaires: 32 900 euros pour les autres prestations de services. (34 900 € plafond majoré)</p>
+      <p><span class="glyphicon glyphicon-chevron-right"></span>&nbsp; Si votre chiffre d’affaires de l’année 2013 est supérieur à 50 % des seuils du régime micro-fiscal soit : 16 450 € de CA pour les prestations de service ou activités libérales, Vous devez effectuer la déclaration et le paiement de vos cotisations sur <a href="http://www.lautoentrepreneur.fr">www.lautoentrepreneur.fr*</a>. Si vous ne respectez pas cette obligation, une pénalité vous sera appliquée. </p>    
+  </div><!-- /.col-xs-12 main -->
+</div><!--/.row-->
